2f1812245e8aeefbc510d696a974a9d2a1221d8e
YuriAPCarvalho <yuri.apcarvalho@gmail.com>
2023-08-28 00:12:09 -0400

    rota free

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const root = createRoot(container); // createRoot(container!) if you use TypeScr
 root.render(
   <StrictMode>
     <ReduxProvider store={store}>
-      <BrowserRouter basename="/home">
+      <BrowserRouter basename="/free">
         <App />
       </BrowserRouter>
     </ReduxProvider>
