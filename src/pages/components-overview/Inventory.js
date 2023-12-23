b623bcde3b3b74bc382fac4d4e08a885b69479fd
YuriAPCarvalho <yuri.apcarvalho@gmail.com>
2023-12-23 00:34:58 -0400

    ajuste pdf

--- src/pages/components-overview/Inventory.js
+++ src/pages/components-overview/Inventory.js
@@ -17,6 +17,8 @@ import {
   MenuItem,
   TextField
 } from '@mui/material';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 const Inventory = () => {
   const navigate = useNavigate();
@@ -109,6 +111,9 @@ const Inventory = () => {
 
       await Promise.all(saveRequests);
 
+      // Geração do PDF
+      generatePDF();
+
       console.log('Inventário salvo com sucesso!');
       setInventoryStarted(false);
 
@@ -135,6 +140,64 @@ const Inventory = () => {
       ...prevSaldos,
       [productId]: newSaldoValue
     }));
+  };
+
+  const generatePDF = () => {
+    const pdf = new jsPDF();
+    const fontSize = 10;
+
+    // Adicione o cabeçalho com a imagem e o texto
+    const logoPath = 'https://i.ibb.co/Qfx8nnJ/logo.png';
+    const logoWidth = 30;
+    const logoHeight = 30;
+
+    // Calcula a largura total do documento para centralizar o texto
+    const totalWidth = pdf.internal.pageSize.width;
+
+    // Calcula a posição x para centralizar o texto
+    const textX = (totalWidth - logoWidth) / 1.5;
+
+    pdf.addImage(logoPath, 'PNG', 20, 10, logoWidth, logoHeight);
+
+    pdf.setFontSize(14);
+
+    // Adicione o texto centralizado
+    pdf.text('STOK - Inventário Inteligente', textX, 30, { align: 'center' });
+
+    // Adicione a data e hora abaixo do cabeçalho
+    const currentDateTime = new Date().toLocaleString();
+    pdf.setFontSize(fontSize);
+    pdf.text(`Data e Hora: ${currentDateTime}`, 89, 35);
+
+    const columns = ['Produto ID', 'Descrição Produto', 'Descrição Estoque', 'Saldo Atual', 'Novo Saldo'];
+    const data = products.map((product) => [
+      product.produtoId,
+      product.descricaoProduto,
+      product.descricaoEstoque,
+      product.saldo,
+      newSaldos[product.produtoId] || ''
+    ]);
+
+    pdf.autoTable({
+      startY: 50, // Ajuste conforme necessário
+      head: [columns],
+      body: data,
+      theme: 'striped',
+      styles: {
+        fontSize: fontSize,
+        cellPadding: 2,
+        overflow: 'linebreak'
+      },
+      columnStyles: {
+        0: { columnWidth: 25 },
+        1: { columnWidth: 50 },
+        2: { columnWidth: 55 },
+        3: { columnWidth: 25 },
+        4: { columnWidth: 25 }
+      }
+    });
+
+    pdf.save('relatorio_inventario.pdf');
   };
 
   return (
@@ -182,6 +245,9 @@ const Inventory = () => {
                 Iniciar Inventário
               </Button>
             )}
+            <Button variant="contained" onClick={generatePDF} fullWidth>
+              Gerar Relatório PDF
+            </Button>
           </Box>
         </Grid>
       </Grid>
